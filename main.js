79e2bbb97626d2273d6ed13a4b0be76529b1775e
JuanCaceffo <caceffojuan@gmail.com>
2023-08-01 18:55:50 -0300

    agregadod de evento de inicio de contador y reseteo

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+//DOM elements/nodes
+const stop_start = document.getElementById("start-stop")
+const bottonReset = document.getElementById("reset")
+var countNumber = document.getElementById("counter")
+let isStarted = false
 
-const Conunter = {
-    count: document.querySelector(".card__counter"),
-
-    
-    
+function startStopCount(){
+    isStarted = !isStarted
+    while (isStarted && countNumber.innerText < 9000){
+        countNumber.innerText++
+    }
 }
+function reset(){
+    countNumber.innerText = 0
+}
+
+
+//DOM Events
+stop_start.addEventListener("click",startStopCount)
+bottonReset.addEventListener("click",reset)
 
