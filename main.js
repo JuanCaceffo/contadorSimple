93d9e571f516013a070369741c100af70f0de674
JuanCaceffo <caceffojuan@gmail.com>
2023-08-01 21:05:14 -0300

    contador en funcionamiento

--- main.js
+++ main.js
@@ -2,13 +2,20 @@
 const stop_start = document.getElementById("start-stop")
 const bottonReset = document.getElementById("reset")
 var countNumber = document.getElementById("counter")
-let isStarted = false
+var INTERVAL_ID = null
+
 
 function startStopCount(){
-    isStarted = !isStarted
-    while (isStarted && countNumber.innerText < 9000){
-        countNumber.innerText++
+    const INTERVAL = 1000
+    if (!INTERVAL_ID){
+        INTERVAL_ID = setInterval(count,INTERVAL)
+    }else{
+        clearInterval(INTERVAL_ID)
+        INTERVAL_ID = null
     }
+}
+function count(){
+    countNumber.innerText++
 }
 function reset(){
     countNumber.innerText = 0
